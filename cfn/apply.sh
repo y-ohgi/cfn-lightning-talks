db8f8b22bde6ed97abcbd4f54db532c5138c5a99
y-ohgi <ohgi.211@gmail.com>
2017-10-22 19:45:50 +0900

    fix ディレクトリ構成の変更

--- cfn/apply.sh
+++ cfn/apply.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-BUCKET=ohgi-cfn
 
+TARGET=$1
+ACTION=$2
+
+BUCKET=ohgi-cfn
 aws s3 sync ./stack s3://${BUCKET}/stack
 aws s3 sync ./templates s3://${BUCKET}/templates
 
-aws cloudformation update-stack \
+aws cloudformation ${ACTION}-stack \
     --stack-name hoge \
-    --template-url https://s3-ap-northeast-1.amazonaws.com/ohgi-cfn/templates/securitygroups.yaml \
+    --template-url https://s3-ap-northeast-1.amazonaws.com/${BUCKET}/templates/${TARGET}.yaml \
     --parameters file://parameters/securitygroups.json
-#    --capabilities
+#    --capabilities CAPABILITY_IAM
